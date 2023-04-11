e62b965f29b0fddbe42e72cf34ef1ddc116d3266
Shahabr99 <sean.rohani9@gmail.com>
2023-04-10 20:31:13 -0700

    testing

--- test_app.py
+++ test_app.py
@@ -6,6 +6,6 @@ from app import app
 def test_get_data(self):
     with app.test_client() as client:
 
-        resp = client.post('/result', daa= {'old': 'USD', 'to': 'GBR', 'amount': 5000})
+        resp = client.post('/result', data= {'old': 'USD', 'to': 'GBR', 'amount': 5000})
         html = resp.get_data(as_text=True)
         self.assertEqual(response.status_code, 200)
