e65414988dcb9e2c3664d8de87f042b5cf899136
Shahabr99 <sean.rohani9@gmail.com>
2023-04-10 23:56:02 -0700

    still working on test

--- test_app.py
+++ test_app.py
@@ -1,11 +1,19 @@
 from app import get_data
 from app import app
+from unittest import TestCase
+from flask_debugtoolbar import DebugToolbarExtension
 
 
 
-def test_get_data(self):
-    with app.test_client() as client:
+class FlaskAppTest(TestCase):
+    def setUp(self):
+        """before test setup"""
+        self.client = app.test_client()
+        app.config['TESTING'] = True
 
-        resp = client.post('/result', data= {'old': 'USD', 'to': 'GBR', 'amount': 5000})
-        html = resp.get_data(as_text=True)
-        self.assertEqual(response.status_code, 200)
+    def test_home():
+        resp = client.get('/')
+        self.assertEqual(resp.status_code, 200)
+
+    def test_get_data():
+        
